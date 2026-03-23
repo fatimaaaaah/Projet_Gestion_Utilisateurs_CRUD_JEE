9dd85e6a99be71ea36948c85a59f9b2d128beb94
fatimaaaaah <128695578+fatimaaaaah@users.noreply.github.com>
2026-03-23 20:15:33 +0000

    Refactor AddUser servlet methods and comments

--- src/main/java/servlets/AddUser.java
+++ src/main/java/servlets/AddUser.java
@@ -12,12 +12,10 @@ import dao.UtilisateurDao;
 @WebServlet("/Add")
 public class AddUser extends HttpServlet {
 
-    // Affiche le formulaire AddUser
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         getServletContext().getRequestDispatcher("/WEB-INF/addUser.jsp").forward(request, response);
     }
 
-    // Traite l'ajout quand l'utilisateur soumet le formulaire
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String nom = request.getParameter("nom");
         String prenom = request.getParameter("prenom");
@@ -27,11 +25,9 @@ public class AddUser extends HttpServlet {
         Utilisateurs user = new Utilisateurs(nom, prenom, login, password);
         UtilisateurDao.add(user);
 
-        // stocke le message dans la session
         request.getSession().setAttribute("message", "Utilisateur ajouté avec succès !");
         request.getSession().setAttribute("messageType", "success");
 
-        // redirige vers la liste
         response.sendRedirect("List");
     }
 }
